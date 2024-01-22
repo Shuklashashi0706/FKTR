7c98fc05857ea6a15539f2a773236c8227186c41
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-01-22 17:27:36 +0530

    Updated

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import database from "./data/data.js";
 dotenv.config({path:"./.env"});
-// import {userRegister} from "./controller/User/user.js";
 import { pushContract } from "./controller/Contract/contract.js";
-import { monitorContract } from "./controller/Contract/monitorContract.js";
 const PORT = process.env.PORT;
 
 const app = express();
